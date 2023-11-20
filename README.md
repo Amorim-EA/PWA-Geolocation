e4e3cedd3d95d8649848167a0e5e3e2d2b555811
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-11-20 17:31:28 -0400

    uploads archives

---
+++ README.md
@@ -0,0 +1,4 @@
+# PWA-Geolocation
+
+## Link: 
+- 
---
+++ js/main.js
@@ -0,0 +1,48 @@
+//registrando a service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      let reg;
+      reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+      console.log('Service worker registrada! ', reg);
+    } catch (err) {
+      console.log(' Service worker registro falhou: ', err);
+    }
+  });
+}
+
+let posicaoInicial;//variavel para capturar a posicao
+const capturarLocalizacao = document.getElementById('localizacao');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+const iframeCode = ``
+
+const sucesso = (posicao) => {//callback de sucesso para captura da posicao
+  posicaoInicial = posicao;
+  latitude.innerHTML = posicaoInicial.coords.latitude;
+  longitude.innerHTML = posicaoInicial.coords.longitude;
+  document.querySelector(".mapa").src = `https://www.google.com/maps?q=${posicaoInicial.coords.latitude},${posicaoInicial.coords.longitude}&z=16&output=embed`;
+};
+
+const erro = (error) => {//callback de error (falha para captura de localizacao)
+  let errorMessage;
+  switch(error.code){
+    case 0:
+      errorMessage = "Erro desconhecido"
+    break;
+    case 1:
+      errorMessage = "Permissão negada!"
+    break;
+    case 2:
+      errorMessage = "Captura de posição indisponível!"
+    break;
+    case 3:
+      errorMessage = "Tempo de solicitação excedido!"
+    break;
+  }
+  console.log({ 'Ocorreu um erro: ': errorMessage});
+};
+  
+capturarLocalizacao.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition(sucesso, erro);
+});
